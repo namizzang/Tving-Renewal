39353404bfd6cf5da4c2c0672344d3d44b5042c7
hyun nam cho <gusska158@gmail.com>
2022-08-22 01:53:12 +0900

    plugin, script, image update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded",function(){
+   var tivingCon = document.querySelector('.tivingCon'),
+      slider = document.querySelector('.tivingCon ul'),
+      slide = document.querySelectorAll('.tivingCon ul li');
    
+   for(var i = 0; i < slide.length; i++){
+      slide.style.left = i * 100 + '%';
+   }
 });
