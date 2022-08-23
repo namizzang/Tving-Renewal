86fb70bff1795b9e87cf4a1172935ad3ab1ed21a
hyun nam cho <gusska158@gmail.com>
2022-08-23 17:22:21 +0900

    container complete

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@ var $timeBest = document.querySelector('#timeBest .best'),
    $slider = document.querySelector('#timeBest ul'),
    $slide = document.querySelectorAll('#timeBest ul li'),
    $slideHeight = 0;
-
+``
    for(var i = 0; i < $slide.length; i++){
       $slide[i].style.left = i * 21 + '%';
    }
+
+$(function(){//channel (jQuery ver)
+   var $channel = $('#channel'),
+      $chaSlider = $channel.find('ul'),
+      $chaSlide = $channel.find('li');
+   
+   $chaSlide.each(function(e){
+      $(this).css({left : e * 31 + '%'});
+   });
+});
