345a8eddbcd0ed0ef03a101cf494b2cc5144e1cf
hyun nam cho <gusska158@gmail.com>
2022-08-26 21:36:00 +0900

    index file update

---
+++ index.php
@@ -0,0 +1,469 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8"/>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" minimum="1.0"/>
+   <link rel="stylesheet" href="css/reset.css">
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="plugin/slick-theme.css">
+   <link rel="stylesheet" href="plugin/slick.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   <script src="plugin/slick.min.js"></script>
+</head>
+
+<body>
+	<div id="wrap">
+      <header id="header">
+         <div class="inner">
+            <h1 id="logo">
+               <a href="#" data-image="resource/img">
+                  <img src="resource/img/logo.svg" alt="티빙"/>
+               </a>
+            </h1>
+            <ul id="gnb">
+               <li><a href="#">실시간</a></li>
+               <li><a href="#">TV프로그램</a></li>
+               <li><a href="#">영화</a></li>
+            </ul>
+         </div>
+      </header><!--header-->
+
+      <div id="container">
+         <div id="slideArea">
+            
+            <ul class="one-time">
+               <li>
+                  <p class="s_img"><img src="resource/img/main0.jpg" alt="메인이미지0"/></p>
+                  <div class="inner">
+                     <div class="txtBox">
+                        <strong>
+                           라스베가스의 매력적인 24시<br>
+                           CSI:라스베가스 시즌1
+                        </strong>
+                        <span>라스베가스의 매력적인 24시를 접할 기회!</span>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <p class="s_img"><img src="resource/img/main1.webp" alt="메인이미지0"/></p>
+                  <div class="inner">
+                     <div class="txtBox">
+                        <strong>
+                           라스베가스의 매력적인 24시<br>
+                           CSI:라스베가스 시즌1
+                        </strong>
+                        <span>라스베가스의 매력적인 24시를 접할 기회!</span>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <p class="s_img"><img src="resource/img/main2.jpg" alt="메인이미지0"/></p>
+                  <div class="inner">
+                     <div class="txtBox">
+                        <strong>
+                           라스베가스의 매력적인 24시<br>
+                           CSI:라스베가스 시즌1
+                        </strong>
+                        <span>라스베가스의 매력적인 24시를 접할 기회!</span>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <p class="s_img"><img src="resource/img/main3.jpg" alt="메인이미지0"/></p>
+                  <div class="inner">
+                     <div class="txtBox">
+                        <strong>
+                           라스베가스의 매력적인 24시<br>
+                           CSI:라스베가스 시즌1
+                        </strong>
+                        <span>라스베가스의 매력적인 24시를 접할 기회!</span>
+                     </div>
+                  </div>
+               </li>
+               <li>
+                  <p class="s_img"><img src="resource/img/main4.jpg" alt="메인이미지0"/></p>
+                  <div class="inner">
+                     <div class="txtBox">
+                        <strong>
+                           라스베가스의 매력적인 24시<br>
+                           CSI:라스베가스 시즌1
+                        </strong>
+                        <span>라스베가스의 매력적인 24시를 접할 기회!</span>
+                     </div>
+                  </div>
+               </li>
+            </ul>
+
+         </div><!--slideArea-->
+         <script>
+            $(function(){
+               $('.one-time').slick({
+                  autoplay:true,
+                  autoplaySpeed:4000,
+                  dots: true,
+                  infinite: true,
+                  speed: 300,
+                  slidesToShow: 1,
+               });
+            });
+         </script>
+         
+
+         <section id="section1">
+            <!--실제 사용 영역-->
+            <div class="inner">
+               <h2>티빙 애니매이션 시리즈</h2>
+               <div class="tivingCon">
+                  <ul>
+                     <li>
+                        <p class="imgArea">
+                           <img src="resource/img/[더빙] 극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들.jpg" alt="포스터이미지01"/>
+                        </p>
+                        <p class="desc">
+                           <em class="">[더빙]</em>
+                           <em class="tit">극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들</em>
+                           <span class="explain">
+                              아이들의 낙서가 사라져 붕괴 위기에 처한 낙서왕국은
+                              낙서 에너지를 모으기 위해 지구 침공을 시작한다.<br>
+                              
+                              과연, 크레용 용사 짱구는 낙서 용사들과 함께
+                              위험에 빠진 떡잎마을과 세계를 구할 수 있을까?!<br>
+                           </span>
+                        </p>
+                     </li>
+                     <li>
+                        <p class="imgArea">
+                           <img src="resource/img/[더빙] 명탐정 코난：눈동자 속의 암살자 - 극장판 4기.jpg" alt="포스터이미지01"/>
+                        </p>
+                        <p class="desc">
+                           <em class="">[더빙]</em>
+                           <em class="tit">극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들</em>
+                           <span class="explain">
+                              아이들의 낙서가 사라져 붕괴 위기에 처한 낙서왕국은
+                              낙서 에너지를 모으기 위해 지구 침공을 시작한다.<br>
+                              
+                              과연, 크레용 용사 짱구는 낙서 용사들과 함께
+                              위험에 빠진 떡잎마을과 세계를 구할 수 있을까?!<br>
+                           </span>
+                        </p>
+                     </li>
+                     <li>
+                        <p class="imgArea">
+                           <img src="resource/img/[더빙] 명탐정 코난：비색의 탄환.jpg" alt="포스터이미지01"/>
+                        </p>
+                        <p class="desc">
+                           <em class="">[더빙]</em>
+                           <em class="tit">극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들</em>
+                           <span class="explain">
+                              아이들의 낙서가 사라져 붕괴 위기에 처한 낙서왕국은
+                              낙서 에너지를 모으기 위해 지구 침공을 시작한다.<br>
+                              
+                              과연, 크레용 용사 짱구는 낙서 용사들과 함께
+                              위험에 빠진 떡잎마을과 세계를 구할 수 있을까?!<br>
+                           </span>
+                        </p>
+                     </li>
+                     <li>
+                        <p class="imgArea">
+                           <img src="resource/img/[더빙] 명탐정 코난：칠흑의 추적자 - 극장판 13기.jpg" alt="포스터이미지01"/>
+                        </p>
+                        <p class="desc">
+                           <em class="">[더빙]</em>
+                           <em class="tit">극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들</em>
+                           <span class="explain">
+                              아이들의 낙서가 사라져 붕괴 위기에 처한 낙서왕국은
+                              낙서 에너지를 모으기 위해 지구 침공을 시작한다.<br>
+                              
+                              과연, 크레용 용사 짱구는 낙서 용사들과 함께
+                              위험에 빠진 떡잎마을과 세계를 구할 수 있을까?!<br>
+                           </span>
+                        </p>
+                     </li>
+                     <li>
+                        <p class="imgArea">
+                           <img src="resource/img/[더빙] 명탐정코난 대괴수 고메라 VS 가면사나이.jpg" alt="포스터이미지01"/>
+                        </p>
+                        <p class="desc">
+                           <em>[더빙]</em>
+                           <em class="tit">극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들</em>
+                           <span class="explain">
+                              아이들의 낙서가 사라져 붕괴 위기에 처한 낙서왕국은
+                              낙서 에너지를 모으기 위해 지구 침공을 시작한다.<br>
+                              
+                              과연, 크레용 용사 짱구는 낙서 용사들과 함께
+                              위험에 빠진 떡잎마을과 세계를 구할 수 있을까?!<br>
+                           </span>
+                        </p>
+                     </li>
+                     <li>
+                        <p class="imgArea">
+                           <img src="resource/img/[더빙] 명탐정코난 피아노 소나타 월광 살인사건.jpg" alt="포스터이미지01"/>
+                        </p>
+                        <p class="desc">
+                           <em class="">[더빙]</em>
+                           <em class="tit">극장판 짱구는 못말려：격돌_ 낙서왕국과 얼추 네 명의 용사들</em>
+                           <span class="explain">
+                              아이들의 낙서가 사라져 붕괴 위기에 처한 낙서왕국은
+                              낙서 에너지를 모으기 위해 지구 침공을 시작한다.<br/>
+                              과연, 크레용 용사 짱구는 낙서 용사들과 함께
+                              위험에 빠진 떡잎마을과 세계를 구할 수 있을까?!<br/>
+                           </span>
+                        </p>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </section>
+         
+         <article id="event">
+            <div>
+               <img src="resource/img/-1.jpg" alt="이벤트이미지"/>
+            </div>
+            <div class="btnArea"></div>
+         </article>
+
+         <article id="tvingSoon">
+            <div class="bg">
+               <!-- <img src="resource/img/" alt=""> -->
+            </div>
+            <p class="commingImg">
+               <img src="resource/img/tving_soon_1.png" alt="">
+            </p>
+         </article>
+
+         <section id="bestContent">
+            <div class="inner">
+               <h2>
+                  조현남님이 시청중인 컨텐츠
+               </h2>
+               <div class="btCon">
+                  <ul>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지00"/>
+                        </p>
+                     </li>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지01"/>
+                        </p>
+                     </li>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지01"/>
+                        </p>
+                     </li>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지01"/>
+                        </p>
+                     </li>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지01"/>
+                        </p>
+                     </li>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지01"/>
+                        </p>
+                     </li>
+                     <li>
+                        <p>
+                           <img src="resource/img/[무삭제판] 배틀로얄.png" alt="포스터이미지01"/>
+                        </p>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </section>
+
+         <section id="timeBest">
+            <div class="inner">
+               <h2>실시간 인기 영화</h2>
+               <div class="best">
+                  <ul>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (1).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">1</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (10).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">2</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (100).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">3</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (11).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">4</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (12).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">5</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (13).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">6</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (14).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">7</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (15).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">8</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (18).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">9</p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/F_webp_400 (22).jpg" alt="best0">
+                        </p>
+                        <p class="ranking">10</p>
+                     </li>
+                  </ul>
+               </div>   
+            </div>   
+         </section><!--timeBest-->
+         <article id="using">
+            <div class="numArea">
+               <p>
+                  <em class="usingTit">티빙 가입자</em>
+                  <em class="usingNum" data-num = "434043021">434,043,021</em>
+               </p>
+               <p>
+                  <em class="usingTit">티빙 오리지널 컨텐츠</em>
+                  <em class="usingNum" data-num = "3403">3,403</em>
+               </p>
+               <p>
+                  <em class="usingTit">이번달 신규 컨텐츠</em>
+                  <em class="usingNum" data-num = "20">20</em>
+               </p>
+               <p>
+                  <em class="usingTit">티빙 OTT 서비스 순위</em>
+                  <em class="usingNum" data-num = "1">1</em>
+               </p>
+            </div>
+         </article><!--using-->
+
+         <section id="channel">
+            <div class="inner">
+               <h2>고민없이 보는 밥친구 채널</h2>
+               <div class="chanList">
+                  <ul>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/CH.노는언니.png" alt="best0">
+                        </p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/CH.무엇이든 물어보살.jpg" alt="best0">
+                        </p>
+                     </li>
+                     <!-- <li>
+                        <p class="img">
+                           <img src="resource/img/CH.비디오스타.jpg" alt="best0">
+                        </p>
+                     </li> -->
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/CH.어서와 한국은 처음이지.jpg" alt="best0">
+                        </p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/CH.연애의 참견 시즌3.png" alt="best0">
+                        </p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/CH.요트원정대.png" alt="best0">
+                        </p>
+                     </li>
+                     <li>
+                        <p class="img">
+                           <img src="resource/img/CH.이십세기 힛-트쏭.png" alt="best0">
+                        </p>
+                     </li>
+                  </ul>
+               </div>   
+            </div>   
+         </section><!--timeBest-->
+      </div><!-- container -->
+      
+      <footer id="footer">
+         <div class="inner">
+            <div class="topInfo clear">
+               <div class="notice">
+                  <h3>공지사항</h3>
+                  <div class="noticeBox">
+                     <ul>
+                        <li><a href="#none">[작업공지] 서버 점검 작업 안내 (2022년 8월 24일 수 03:00~06:00)</a></li>
+                        <li><a href="#none">[작업공지] 1</a></li>
+                        <li><a href="#none">[작업공지] 2</a></li>
+                        <li><a href="#none">[작업공지] 3</a></li>
+                        <li><a href="#none">[작업공지] 4</a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="brandShop">
+
+               </div>
+               <div class="group">
+                  
+               </div>
+            </div><!--topInfo-->
+
+            <nav class="util">
+               <a href="#none">고객센터</a>
+               <a href="#none">이용약관</a>
+               <a href="#none"><strong>개인정보방침</strong></a>
+               <a href="#none">청소년 보호정책</a>
+               <a href="#none">법적고지</a>
+               <a href="#none">이벤트</a>
+               <a href="#none">채용 센터</a>
+            </nav><!--util-->
+
+            <div class="sns">
+               <a href="#none"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+               <a href="#none"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+               <a href="#none"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+               <a href="#none"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+
+            </div>
+
+            <div class="copyright">
+               <a href=""><strong>Cho Hyun Nam &copy; wiwi154@naver.com</strong></a>
+            </div><!--copyright-->
+            
+         </div>
+      </footer><!--footer-->
+   </div>
+   <script src="js/main.js"></script>
+</body>    
+</html>
